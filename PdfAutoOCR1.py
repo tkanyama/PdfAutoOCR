739379eeea5e6b380d584effd9fe1ab6ab21d11c
tkanyama <toshi.kanyama@gmail.com>
2023-01-17 14:13:17 +0900

    first commit

---
+++ PdfAutoOCR1.py
@@ -0,0 +1,227 @@
+import os
+import sys
+import cv2
+from PyPDF2 import PdfReader
+from pdf2image import convert_from_path, convert_from_bytes
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import pyocr
+import re
+
+def isfloat(s):  # 浮動小数点数値を表しているかどうかを判定
+    try:
+        float(s)  # 文字列を実際にfloat関数で変換してみる
+    except ValueError:
+        return False
+    else:
+        return True
+
+
+
+#OCRエンジンを取得する
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print("OCRエンジンが指定されていません")
+    sys.exit(1)
+else:
+    tool = tools[0]
+# print(text)
+pdfFileName = "サンプル計算書(1).pdf"
+with open(pdfFileName, "rb") as input:
+    reader = PdfReader(input)
+    # pdfの総ページ数は？
+    print("サンプル計算書(1).pdf has %d pages.\n" % len(reader.pages))
+    # 指定のページのデータを読み込む
+    images = convert_from_path(pdfFileName,dpi=400,first_page=186,last_page=186)
+    # img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # retval, img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    for image in images:
+        img = np.array(image)
+        img2 = img
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.show()
+
+        # モノクロ・グレースケール画像へ変換（2値化前の画像処理）
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # 2値化（Binarization）：白（1）黒（0）のシンプルな2値画像に変換
+        retval, img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        # plt.imshow(img_binary)
+        print('【直線を検出中・・・】2値化処理画像 - Binarization')
+        # plt.imshow(cv2.cvtColor(img_binary, cv2.COLOR_BGR2RGB))
+        # plt.show()
+
+        lines = cv2.HoughLinesP(img_binary, rho=1, theta=np.pi/360, threshold=15, minLineLength=60, maxLineGap=5.4)
+
+        if lines is None:  # 直線が検出されない場合
+            print('\n【直線の検出結果】')
+            print('　直線は検出されませんでした。')
+            # file_name = os.path.splitext(os.path.basename(input_file))[0]
+            # cv2.imwrite(f'line_cut_{file_name}.png', img)
+        else:  # 直線が検出された場合
+            print('\n【直線の検出結果】')
+            print('　直線が検出されました。検出した直線を削除します。')
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                # 検出した直線に赤線を引く
+                red_lines_img = cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+            print('\n【直線検出部位の視覚化】')
+            print('　赤色部分が検出できた直線。')
+            # plt.imshow(cv2.cvtColor(red_lines_img, cv2.COLOR_BGR2RGB))
+            # plt.show()
+
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                # 検出した直線を消す（白で線を引く）：2値化した際に黒で表示される
+                no_lines_img = cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 1)
+
+                # 直線を除去した画像を元のファイル名の頭に「line_cut_」をつけて保存。「0」を指定でファイル名を取得
+                # file_name = os.path.splitext(os.path.basename(input_file))[0]
+                # cv2.imwrite(f'line_cut_{file_name}.png', no_lines_img)
+            print('\n【直線検出部位の削除結果：元の画像から削除】')
+            print('　白色部分が検出した直線を消した場所（背景が白の場合は区別できません）。')
+            # plt.imshow(cv2.cvtColor(no_lines_img, cv2.COLOR_BGR2RGB))
+            # plt.show()
+
+        image2 = Image.fromarray(no_lines_img)
+
+        img = np.array(image2)
+        # plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        # plt.show()
+
+        # モノクロ・グレースケール画像へ変換（2値化前の画像処理）
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # 2値化（Binarization）：白（1）黒（0）のシンプルな2値画像に変換
+        retval, img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        # plt.imshow(img_binary)
+        print('【直線を検出中・・・】2値化処理画像 - Binarization')
+        # plt.imshow(cv2.cvtColor(img_binary, cv2.COLOR_BGR2RGB))
+        # plt.show()
+
+        lines = cv2.HoughLinesP(img_binary, rho=1, theta=np.pi/360, threshold=15, minLineLength=60, maxLineGap=5.4)
+
+        if lines is None:  # 直線が検出されない場合
+            print('\n【直線の検出結果】')
+            print('　直線は検出されませんでした。')
+            # file_name = os.path.splitext(os.path.basename(input_file))[0]
+            # cv2.imwrite(f'line_cut_{file_name}.png', img)
+        else:  # 直線が検出された場合
+            print('\n【直線の検出結果】')
+            print('　直線が検出されました。検出した直線を削除します。')
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                # 検出した直線に赤線を引く
+                red_lines_img = cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+            print('\n【直線検出部位の視覚化】')
+            print('　赤色部分が検出できた直線。')
+            # plt.imshow(cv2.cvtColor(red_lines_img, cv2.COLOR_BGR2RGB))
+            # plt.show()
+
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                # 検出した直線を消す（白で線を引く）：2値化した際に黒で表示される
+                no_lines_img = cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 1)
+
+                # 直線を除去した画像を元のファイル名の頭に「line_cut_」をつけて保存。「0」を指定でファイル名を取得
+                # file_name = os.path.splitext(os.path.basename(input_file))[0]
+                # cv2.imwrite(f'line_cut_{file_name}.png', no_lines_img)
+            print('\n【直線検出部位の削除結果：元の画像から削除】')
+            print('　白色部分が検出した直線を消した場所（背景が白の場合は区別できません）。')
+            # plt.imshow(cv2.cvtColor(no_lines_img, cv2.COLOR_BGR2RGB))
+            # plt.show()
+
+
+        image2 = Image.fromarray(no_lines_img)
+
+
+        # 読み込んだページのテキストを抽出
+        # print(extract_text(page))
+        #文字と座標を読み取る
+        box_builder = pyocr.builders.WordBoxBuilder(tesseract_layout=11)
+        text_position = tool.image_to_string(image2,lang="eng",builder=box_builder)
+
+        #取得した座標と文字を出力するし、画像に枠を書き込む/\©
+        img2 = np.array(image)
+        t0 = ""
+        p0 = [0,0]
+        p1 = [0,0]
+        flag = False
+        limit = 0.90
+        for res in text_position:
+            m = res.content
+            print(m)
+            m = m.replace(",",".").replace("@","0.").replace("/","").replace("\\","").replace("©","0.").replace("Q","0")
+            m = m.replace("Q","0").replace("U","0").replace("P","0").replace(' ', '').replace('ha', '42').replace('eo', '70').replace('oe', '0.')
+            m = m.replace("o","").replace("-","")
+            if m == "(0" or m == "0":
+                m = m + "."
+            if m == "(0." or m == "0.":
+                t0 = m
+                p0 = res.position[0]
+                flag = True
+            else:
+                t = t0 + m
+                t0 = ""
+                if flag == False:
+                    p0 = res.position[0]
+                p1 = res.position[1]
+                flag = False
+                if re.search(r'\d', t):
+                    # print(m)
+                    # print(res.position)
+                    if isfloat(t.replace("(","").replace(")","")):
+                        a = float(t.replace("(","").replace(")",""))
+                        print(a)
+                        if a >= limit and a < 1.0 :
+                            # cv2.rectangle(img2,res.position[0],res.position[1],(255,0,0),1)
+                            cv2.rectangle(img2,p0,p1,(255,0,0),1)
+                        elif a >= 1.0 and a <=99.0:
+                            b = a / 100.0
+                            if b >= limit and b < 1.0 :
+                                # p2 = p0
+                                # p2[0] = p0[0]-(p1[0]-p0[0])
+                                cv2.rectangle(img2,p0,p1,(255,0,0),1)
+                        elif a >= 101 and a <=999:
+                            b = a / 1000.0
+                            if b >= limit and b < 1.0 :
+                                # p2 = p0
+                                # p2[0] = p0[0]-(p1[0]-p0[0])
+                                cv2.rectangle(img2,p0,p1,(255,0,0),1)
+        
+        plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+        plt.show()
+
+        pil_image = Image.fromarray(img2)
+        im_pdf = pil_image.convert("RGB")
+        im_pdf.save(r"test.pdf")
+
+# from pdfminer.pdfinterp import PDFResourceManager
+# from pdfminer.converter import TextConverter
+# from pdfminer.pdfinterp import PDFPageInterpreter
+# from pdfminer.pdfpage import PDFPage
+# from pdfminer.layout import LAParams
+# from io import StringIO
+
+# pdf_file_path = "page179.pdf"
+
+# with open(pdf_file_path , "rb") as pdf_file: #ファイルオブジェクトを受け取り、変数「pdf_file」に代入。
+#     output = StringIO() #コンソールに出力されたテキストを取得するため、IOクラス「StringIO」使用
+#     resource_manager = PDFResourceManager()
+#     laparams = LAParams()
+#     #レイアウトの変更がなければデフォルトのままで
+#     text_converter = TextConverter(resource_manager, output, laparams=laparams)
+#     page_interpreter = PDFPageInterpreter(resource_manager, text_converter)
+
+#     for i_page in PDFPage.get_pages(pdf_file): #1ベージずづ処理 
+#         page_interpreter.process_page(i_page)
+#         # break
+#     # i_page = PDFPage.get_pages(pdf_file)
+#     # page_interpreter.process_page(i_page[179])
+
+#     output_text = output.getvalue()
+#     output.close()
+#     text_converter.close()
+
+# print(output_text)
